572d7f39dda8be5fbc15bf5b9801fe5a94066c63
Moltenship <40624720+Moltenship@users.noreply.github.com>
2018-12-16 19:11:37 +0700

    Add files via upload

---
+++ lab5eng.cpp
@@ -0,0 +1,11 @@
+﻿// lab5eng.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+	printf("Hello world!");
+}
+
